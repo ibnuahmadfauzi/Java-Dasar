72c73163d472575a205e6f330add4a4c4b0dd0b5
ibnuahmadfauzi <59650987+ibnuahmadfauzi@users.noreply.github.com>
2020-04-24 10:42:33 +0700

    IbnuAhmadFauzi
    Membuat sintaks dasar dalam bahasa pemrograman JAVA

---
+++ 2 Sintaks Dasar/Main.java
@@ -0,0 +1,18 @@
+// {  } adalah badan program
+// file .java harus memiliki keyword public class
+// public adalah visibility agar program terbuka untuk umum
+// class adalah keyword untuk pembuatan class
+// Main adalah penamaan class dan nama class harus sama dengan nama file dan huruf depannya harus besar
+public class Main{
+
+// public static merupakan keyword untuk java
+// main merupakan keyword default java untuk menjalankan program
+	public static void main(String[] args){
+		// System merupakan perintah dimana kita akan membuat program di sistem operasi yang digunakan
+		// out merupakan penanda akan mengeluarkan objek di consol
+		// println berfungsi untuk menulis
+		// ("Belajar Java") tulisan yang ditampilkan Belajar Java
+		// Setiap akhir statement diakhiri di ikuti semicolon (;)
+		System.out.println("Belajar Java");
+	}
+}
