73de3e549d58dabbff428e18e07862d3e8e88bbc
ibnuahmadfauzi <59650987+ibnuahmadfauzi@users.noreply.github.com>
2020-04-24 11:13:49 +0700

    24-04-2020
    Membuat deklarasi variabel dalam bahasa pemrograman JAVA

---
+++ 5 Deklarasi Variabel/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Mendeklarasikan Variabel
+		int a, b, c, d, e, f, g;
+
+		// Mengisi nilai variabel
+		a = 10;
+		b = 20;
+		c = 30;
+		d = 40;
+		e = 50;
+		f = 60;
+		g = 70;
+
+		// Menampilkan nilai masing-masing variabel
+		System.out.println("  a = " + a);
+		System.out.println("  b = " + b);
+		System.out.println("  c = " + c);
+		System.out.println("  d = " + d);
+		System.out.println("  e = " + e);
+		System.out.println("  f = " + f);
+		System.out.println("  g = " + g);
+
+	}
+
+}
