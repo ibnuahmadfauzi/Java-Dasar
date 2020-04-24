883f430981134db2ddf8394024ff26a1d6b8f836
ibnuahmadfauzi <59650987+ibnuahmadfauzi@users.noreply.github.com>
2020-04-24 12:51:01 +0700

    24-04-2020
    Tipe data Character & String

---
+++ 8 Tipe data Char dan String/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Tipe data char atau karakter
+		System.out.println(" === CHAR ===");
+		char jenisKelamin = 'L';
+		char golonganDarah = 'O';
+		System.out.println(" Jenis Kelamin  : " + jenisKelamin);
+		System.out.println(" Golongan Darah : " + golonganDarah);
+
+		System.out.println("");
+
+		// Tipe data string
+		System.out.println(" === STRING ===");
+		String namaLengkap = "Ibnu Ahmad Fauzi";
+		String namaPanggilan = "Ibnu";
+		System.out.println(" Nama Lengkap   : " + namaLengkap);
+		System.out.println(" Nama Panggilan : " + namaPanggilan);
+
+	}
+
+}
