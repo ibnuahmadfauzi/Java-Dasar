f8073f90cb4be827f6226eca58d12be7782f865b
ibnuahmadfauzi <59650987+ibnuahmadfauzi@users.noreply.github.com>
2020-04-24 11:08:01 +0700

    24-04-2020
    Membuat variabel dalam bahasa pemrograman JAVA

---
+++ 4 Variabel/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Membuat variabel
+		// TipeData NamaVariabel = Value;
+		String namaLengkap = "Ibnu Ahmad Fauzi";
+		int umur = 19;
+		String agama = "Islam";
+		char jenisKelamin = 'L';
+		boolean sudahMenikah = false;
+
+		// Menampilkan Variabel
+		System.out.println("  Nama Lengkap   : " + namaLengkap);
+		System.out.println("  Umur           : " + umur);
+		System.out.println("  Agama          : " + agama);
+		System.out.println("  Jenis Kelamin  : " + jenisKelamin);
+		System.out.println("  Sudah Menikah  : " + sudahMenikah);
+
+	}
+
+}
