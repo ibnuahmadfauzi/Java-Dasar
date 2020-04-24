f53a560d79a0894a42df1062d3ed062517a310b5
ibnuahmadfauzi <59650987+ibnuahmadfauzi@users.noreply.github.com>
2020-04-24 10:51:02 +0700

    IbnuAhmadFauzi
    Memahami sintaks (String[] args)

---
+++ 3 (String[] args)/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+
+}
+
+// String[] args merupakan input yang bisa kita masukin untuk mengeksekusi java
+// String[] juga merupakan array yaitu tipe yang bertugas untuk mengumpulkan data dan memiliki indeks
