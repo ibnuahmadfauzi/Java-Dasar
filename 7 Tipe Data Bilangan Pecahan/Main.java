fb6d9e0316364a771061fc613b20b56ad140471f
ibnuahmadfauzi <59650987+ibnuahmadfauzi@users.noreply.github.com>
2020-04-24 12:41:07 +0700

    24-04-2020
    Tipe data bilangan pecahan dalam bahasa pemrograman JAVA

---
+++ 7 Tipe Data Bilangan Pecahan/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		// float & double
+
+		// float
+		float phi = 22/7f;
+		System.out.println(" Contoh           : " + phi);
+		System.out.println(" Tipe Data        : float");
+		System.out.println(" Bilangan Maximal : " + Float.MAX_VALUE);
+		System.out.println(" Bilangan Minimal : " + Float.MIN_VALUE);
+		System.out.println(" Besar Data       : " + Float.BYTES + " bytes");
+		System.out.println(" Besar Data       : " + Float.SIZE + " bit");
+
+		System.out.println("");
+
+		// double
+		double phi2 = 22/7f;
+		System.out.println(" Contoh           : " + phi2);
+		System.out.println(" Tipe Data        : double");
+		System.out.println(" Bilangan Maximal : " + Double.MAX_VALUE);
+		System.out.println(" Bilangan Minimal : " + Double.MIN_VALUE);
+		System.out.println(" Besar Data       : " + Double.BYTES + " bytes");
+		System.out.println(" Besar Data       : " + Double.SIZE + " bit");
+
+	}
+
+}
