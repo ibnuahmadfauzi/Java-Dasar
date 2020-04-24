6a66e04f07172f5de9725980827d7dbedbdbd645
ibnuahmadfauzi <59650987+ibnuahmadfauzi@users.noreply.github.com>
2020-04-24 12:57:38 +0700

    24-04-2020
    Tipe data boolean dalam bahasa pemrograman JAVA

---
+++ 9 Tipe Data Boolean/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Boolean (true&false)
+		boolean lakiLaki = true;
+		boolean perempuan = false;
+		System.out.println("Laki - Laki : " + lakiLaki);
+		System.out.println("Perempuan   : " + perempuan);
+
+	}
+
+}
