2e1187f9f9b242fd66ba34dedcdfc37bfd85c9b4
ibnuahmadfauzi <59650987+ibnuahmadfauzi@users.noreply.github.com>
2020-04-24 11:26:43 +0700

    24-04-2020
    Tipe Data Bilangan Bulat
     byte >> short >> integer >> long

---
+++ 6 Tipe Data Bilangan Bulat/Main.java
@@ -0,0 +1,49 @@
+public class Main{
+
+	public static void main(String[] args){
+
+		/*
+		Urutan tipe data bilangan bulat berdasarkan besar data
+		1. byte
+		2. short
+		3. int
+		4. long
+		*/
+
+		// byte
+		System.out.println(" Tipe Data        : byte");
+		System.out.println(" Bilangan Maximal : " + Byte.MAX_VALUE);
+		System.out.println(" Bilangan Minimal : " + Byte.MIN_VALUE);
+		System.out.println(" Besar Data       : " + Byte.BYTES + " bytes");
+		System.out.println(" Besar Data       : " + Byte.SIZE + " bit");
+
+		System.out.println("");
+
+		// short
+		System.out.println(" Tipe Data        : short");
+		System.out.println(" Bilangan Maximal : " + Short.MAX_VALUE);
+		System.out.println(" Bilangan Minimal : " + Short.MIN_VALUE);
+		System.out.println(" Besar Data       : " + Short.BYTES + " bytes");
+		System.out.println(" Besar Data       : " + Short.SIZE + " bit");
+
+		System.out.println("");
+
+		// int
+		System.out.println(" Tipe Data        : integer");
+		System.out.println(" Bilangan Maximal : " + Integer.MAX_VALUE);
+		System.out.println(" Bilangan Minimal : " + Integer.MIN_VALUE);
+		System.out.println(" Besar Data       : " + Integer.BYTES + " bytes");
+		System.out.println(" Besar Data       : " + Integer.SIZE + " bit");
+
+		System.out.println("");
+
+		// long
+		System.out.println(" Tipe Data        : long");
+		System.out.println(" Bilangan Maximal : " + Long.MAX_VALUE);
+		System.out.println(" Bilangan Minimal : " + Long.MIN_VALUE);
+		System.out.println(" Besar Data       : " + Long.BYTES + " bytes");
+		System.out.println(" Besar Data       : " + Long.SIZE + " bit");
+
+	}
+
+}
