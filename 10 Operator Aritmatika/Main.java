48f79e339dad16823a33fd050b53dff847622d06
ibnuahmadfauzi <59650987+ibnuahmadfauzi@users.noreply.github.com>
2020-04-24 13:10:44 +0700

    24-04-2020
    Operator aritmatika dalam bahasa pemrograman JAVA

---
+++ 10 Operator Aritmatika/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		// OPERATOR ARITMATIKA
+
+		// Penjumlahan (+)
+		int a = 100;
+		int b = 30;
+		int c = (a + b);
+		System.out.println(" === PENJUMLAHAN ===");
+		System.out.println(" " + a + " + " + b + " = " + c);
+
+		System.out.println("");
+
+		// Pengurangan (-)
+		int d = 100;
+		int e = 30;
+		int f = (d - e);
+		System.out.println(" === PENGURANGAN ===");
+		System.out.println(" " + d + " - " + e + " = " + f);
+
+		System.out.println("");
+
+		// Perkalian (*)
+		int g = 100;
+		int h = 30;
+		int i = (g * h);
+		System.out.println(" === PERKALIAN ===");
+		System.out.println(" " + g + " * " + h + " = " + i);
+
+		System.out.println("");
+
+		// Pembagian (/)
+		int j = 100;
+		int k = 30;
+		int l = (j / k);
+		System.out.println(" === PEMBAGIAN ===");
+		System.out.println(" " + j + " / " + k + " = " + l);
+
+		System.out.println("");
+
+		// Modulus (%)
+		int m = 100;
+		int n = 30;
+		int o = (m % n);
+		System.out.println(" === MODULUS ===");
+		System.out.println(" " + m + " % " + n + " = " + o);
+
+	}
+
+}
